18c0aaf9822c745a9f14a0dbef249a2336efd504
SamuelCarnero123 <samuel.carnero.jimenez@gregoriofer.com>
2025-03-05 14:20:34 +0100

    Add files via upload
    terminal con spinners

--- Terminal.java
+++ Terminal.java
@@ -3,8 +3,13 @@ package ClasesPrincipales;
 import java.awt.Color;
 import java.awt.Component;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JSpinner;
 import javax.swing.JTable;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 
@@ -20,12 +25,44 @@ public class Terminal extends javax.swing.JFrame {
     private JFrame pantallaAparcar;
     private JFrame pantallaRetirar;
     private JFrame pantallaPagar;
+    private JFrame pantallaPagarSpinners;
     private Maquina app = new Maquina();
     private Ticket ticketSeleccionado;
     private Ticket ticket;
     private JTable tabla;//control para mostrar la matriz
     private DefaultTableModel modeloTabla;//contenedor de la matriz de enteros
     private Deposito deposito = new Deposito();
+    private SpinnerNumberModel modelBilletes20 = new SpinnerNumberModel(0, 0, 100, 1);  // Billetes de 20
+    private SpinnerNumberModel modelBilletes10 = new SpinnerNumberModel(0, 0, 100, 1);  // Billetes de 10
+    private SpinnerNumberModel modelBilletes5 = new SpinnerNumberModel(0, 0, 100, 1);   // Billetes de 5
+    private SpinnerNumberModel modelMonedas2 = new SpinnerNumberModel(0, 0, 100, 1);   // Monedas de 2
+    private SpinnerNumberModel modelMonedas1 = new SpinnerNumberModel(0, 0, 100, 1);   // Monedas de 1
+    private SpinnerNumberModel modelMonedas05 = new SpinnerNumberModel(0, 0, 100, 1);  // Monedas de 0.5
+    private SpinnerNumberModel modelMonedas02 = new SpinnerNumberModel(0, 0, 100, 1);  // Monedas de 0.2
+    private SpinnerNumberModel modelMonedas01 = new SpinnerNumberModel(0, 0, 100, 1);  // Monedas de 0.1
+    private SpinnerNumberModel modelMonedas005 = new SpinnerNumberModel(0, 0, 100, 1); // Monedas de 0.05
+
+    private JSpinner spinnerBilletes20 = new JSpinner(modelBilletes20);
+    private JSpinner spinnerBilletes10 = new JSpinner(modelBilletes10);
+    private JSpinner spinnerBilletes5 = new JSpinner(modelBilletes5);
+    private JSpinner spinnerMonedas2 = new JSpinner(modelMonedas2);
+    private JSpinner spinnerMonedas1 = new JSpinner(modelMonedas1);
+    private JSpinner spinnerMonedas05 = new JSpinner(modelMonedas05);
+    private JSpinner spinnerMonedas02 = new JSpinner(modelMonedas02);
+    private JSpinner spinnerMonedas01 = new JSpinner(modelMonedas01);
+    private JSpinner spinnerMonedas005 = new JSpinner(modelMonedas005);
+    private JLabel labelBilletes20 = new JLabel("Billetes de 20:");
+    private JLabel labelBilletes10 = new JLabel("Billetes de 10:");
+    private JLabel labelBilletes5 = new JLabel("Billetes de 5:");
+    private JLabel labelMonedas2 = new JLabel("Monedas de 2:");
+    private JLabel labelMonedas1 = new JLabel("Monedas de 1:");
+    private JLabel labelMonedas05 = new JLabel("Monedas de 0.5:");
+    private JLabel labelMonedas02 = new JLabel("Monedas de 0.2:");
+    private JLabel labelMonedas01 = new JLabel("Monedas de 0.1:");
+    private JLabel labelMonedas005 = new JLabel("Monedas de 0.05:");
+    private JLabel labelTotal = new JLabel("Total: 0.00 € ");
+    
+    private double total;
 
     /**
      * Constructor por defecto de la clase Terminal. Este constructor inicializa
@@ -50,6 +87,26 @@ public class Terminal extends javax.swing.JFrame {
         this.setResizable(false);
         modeloTabla = new DefaultTableModel();
         tabla = new JTable(modeloTabla);
+
+        spinnerBilletes20.addChangeListener(changeListener);
+
+        spinnerBilletes10.addChangeListener(changeListener);
+
+        spinnerBilletes5.addChangeListener(changeListener);
+
+        spinnerMonedas2.addChangeListener(changeListener);
+
+        spinnerMonedas1.addChangeListener(changeListener);
+
+        spinnerMonedas05.addChangeListener(changeListener);
+
+        spinnerMonedas02.addChangeListener(changeListener);
+
+        spinnerMonedas01.addChangeListener(changeListener);
+
+        spinnerMonedas005.addChangeListener(changeListener);
+        
+        total = 0;
         actualizarTabla();
         mostrarTabla();
 
@@ -150,9 +207,9 @@ public class Terminal extends javax.swing.JFrame {
         //ventana principal del parquimetro
         setSize(350, 720);
         LabelParquimetroPrincipal.setSize(350, 720);
-        //ScrollTabla.setLayout(null);
+
         setContentPane(LabelParquimetroPrincipal);
-        //setResizable(false);
+
         LabelParquimetroPrincipal.add(BotonAparcarCoche);
         LabelParquimetroPrincipal.add(BotonRetirarCoche);
 
@@ -162,7 +219,6 @@ public class Terminal extends javax.swing.JFrame {
         BotonRetirarCoche.setSize(250, 40);
 
         ScrollTabla.setSize(237, 175);
-        //tabla.setSize(237, 175);
 
         BotonAparcarCoche.setBounds(50, 500, BotonAparcarCoche.getWidth(), BotonAparcarCoche.getHeight());
         BotonRetirarCoche.setBounds(50, 550, BotonRetirarCoche.getWidth(), BotonRetirarCoche.getHeight());
@@ -335,6 +391,136 @@ public class Terminal extends javax.swing.JFrame {
         pantallaPagar.setLocationRelativeTo(null);
     }
 
+    public void ventanaPagarSpinners() {
+        pantallaPagarSpinners = new JFrame();
+        pantallaPagarSpinners.setLayout(null);
+        pantallaPagarSpinners.setResizable(false);
+        pantallaPagarSpinners.setVisible(true);
+        LabelPagarSpinners.setVisible(true);
+        pantallaPagarSpinners.setSize(600, 600);
+        pantallaPagarSpinners.setContentPane(LabelPagarSpinners);
+        LabelPagarSpinners.setSize(600, 600);
+
+        LabelPagarSpinners.add(EditorPane2);
+
+        LabelPagarSpinners.add(labelBilletes20);
+        LabelPagarSpinners.add(spinnerBilletes20);
+
+        LabelPagarSpinners.add(labelBilletes10);
+        LabelPagarSpinners.add(spinnerBilletes10);
+
+        LabelPagarSpinners.add(labelBilletes5);
+        LabelPagarSpinners.add(spinnerBilletes5);
+
+        LabelPagarSpinners.add(labelMonedas2);
+        LabelPagarSpinners.add(spinnerMonedas2);
+
+        LabelPagarSpinners.add(labelMonedas1);
+        LabelPagarSpinners.add(spinnerMonedas1);
+
+        LabelPagarSpinners.add(labelMonedas05);
+        LabelPagarSpinners.add(spinnerMonedas05);
+
+        LabelPagarSpinners.add(labelMonedas02);
+        LabelPagarSpinners.add(spinnerMonedas02);
+
+        LabelPagarSpinners.add(labelMonedas01);
+        LabelPagarSpinners.add(spinnerMonedas01);
+
+        LabelPagarSpinners.add(labelMonedas005);
+        LabelPagarSpinners.add(spinnerMonedas005);
+        
+        LabelPagarSpinners.add(labelTotal);
+        LabelPagarSpinners.add(BotonPagar);
+        
+        BotonPagar.setVisible(true);
+        
+        labelBilletes20.setFont(LabelAñadirMatricula.getFont());
+        labelBilletes10.setFont(LabelAñadirMatricula.getFont());
+        labelBilletes5.setFont(LabelAñadirMatricula.getFont());
+        labelMonedas2.setFont(LabelAñadirMatricula.getFont());
+        labelMonedas1.setFont(LabelAñadirMatricula.getFont());
+        labelMonedas05.setFont(LabelAñadirMatricula.getFont());
+        labelMonedas02.setFont(LabelAñadirMatricula.getFont());
+        labelMonedas01.setFont(LabelAñadirMatricula.getFont());
+        labelMonedas005.setFont(LabelAñadirMatricula.getFont());
+        labelTotal.setFont(LabelAñadirMatricula.getFont());
+        EditorPane2.setFont(LabelAñadirMatricula.getFont());
+        
+
+        EditorPane2.setVisible(true);
+        EditorPane2.setContentType("text/html");
+
+        LabelPagarSpinners.add(labelTotal); // Agregar el JLabel con el total
+        
+        EditorPane2.setSize(300, 90);
+        BotonPagar.setSize(100, 30);
+
+        spinnerBilletes20.setBounds(155, 230, 50, 30);
+        spinnerBilletes10.setBounds(275, 230, 50, 30);
+        spinnerBilletes5.setBounds(395, 230, 50, 30);
+        spinnerMonedas2.setBounds(155, 300, 50, 30);
+        spinnerMonedas1.setBounds(275, 300, 50, 30);
+        spinnerMonedas05.setBounds(395, 300, 50, 30);
+        spinnerMonedas02.setBounds(155, 370, 50, 30);
+        spinnerMonedas01.setBounds(275, 370, 50, 30);
+        spinnerMonedas005.setBounds(395, 370, 50, 30);
+
+        labelBilletes20.setBounds(155, 190, 150, 50);
+        labelBilletes10.setBounds(275, 190, 150, 50);
+        labelBilletes5.setBounds(395, 190, 150, 50);
+        labelMonedas2.setBounds(155, 260, 150, 50);
+        labelMonedas1.setBounds(275, 260, 150, 50);
+        labelMonedas05.setBounds(395, 260, 150, 50);
+        labelMonedas02.setBounds(155, 330, 150, 50);
+        labelMonedas01.setBounds(275, 330, 150, 50);
+        labelMonedas005.setBounds(395, 330, 150, 50);
+        
+        labelTotal.setBounds(260, 420, 200, 50);
+        BotonPagar.setBounds(250, 460, BotonPagar.getWidth(), BotonPagar.getHeight());
+        
+        EditorPane2.setBounds(150, 10, EditorPane2.getWidth(), EditorPane2.getHeight());
+
+        EditorPane2.setText("<html><br>Vehiculo con matricula (" + ticketSeleccionado.getMatricula() + ")</br>"
+                + "<br>Estacionado durante (" + app.calcularTiempoTranscurrido(ticketSeleccionado) + ") minutos</br>"
+                + "<br>El importe correspondiente es (" + app.totalDineroDevolver(app.calcularTiempoTranscurrido(ticketSeleccionado)) + " €)</br></html>");
+        EditorPane2.setEditable(false);
+
+
+        // Hacer visible la ventana
+        LabelPagarSpinners.setVisible(true);
+
+        actualizarTabla();
+        pantallaPagarSpinners.setLocationRelativeTo(null);
+
+    }
+
+    // Agregar ChangeListener a cada JSpinner para actualizar el total
+    protected ChangeListener changeListener = new ChangeListener() {
+        @Override
+        public void stateChanged(ChangeEvent e) {
+            // Obtener los valores de cada JSpinner
+            int billetes20 = (Integer) spinnerBilletes20.getValue();
+            int billetes10 = (Integer) spinnerBilletes10.getValue();
+            int billetes5 = (Integer) spinnerBilletes5.getValue();
+            int monedas2 = (Integer) spinnerMonedas2.getValue();
+            int monedas1 = (Integer) spinnerMonedas1.getValue();
+            int monedas05 = (Integer) spinnerMonedas05.getValue();
+            int monedas02 = (Integer) spinnerMonedas02.getValue();
+            int monedas01 = (Integer) spinnerMonedas01.getValue();
+            int monedas005 = (Integer) spinnerMonedas005.getValue();
+
+            // Calcular el total
+            total = billetes20 * 20.0 + billetes10 * 10.0 + billetes5 * 5.0
+                    + monedas2 * 2.0 + monedas1 * 1.0 + monedas05 * 0.5
+                    + monedas02 * 0.2 + monedas01 * 0.1 + monedas005 * 0.05;
+
+            // Actualizar el JLabel con el total formateado
+            labelTotal.setText(String.format("Total: %.2f € ", total));
+        }
+    };
+    // Agregar el ChangeListener a cada JSpinner
+
     /**
      * Verifica si el valor introducido contiene solo números. Este método
      * intenta convertir el texto introducido en un número. El método también
@@ -490,6 +676,7 @@ public class Terminal extends javax.swing.JFrame {
         jScrollPane2 = new javax.swing.JScrollPane();
         EditorPane2 = new javax.swing.JEditorPane();
         ScrollTabla = new javax.swing.JScrollPane();
+        LabelPagarSpinners = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setMinimumSize(new java.awt.Dimension(365, 755));
@@ -584,6 +771,8 @@ public class Terminal extends javax.swing.JFrame {
         EditorPane2.setFont(new java.awt.Font("Dialog", 0, 14)); // NOI18N
         jScrollPane2.setViewportView(EditorPane2);
 
+        LabelPagarSpinners.setIcon(new javax.swing.ImageIcon(getClass().getResource("/resourcesFotos/Pagar(600x600).png"))); // NOI18N
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -617,16 +806,21 @@ public class Terminal extends javax.swing.JFrame {
                 .addComponent(LabelRetirarCoche, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(48, 48, 48)
                 .addComponent(LabelAparcamiento, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(212, Short.MAX_VALUE))
+                .addGap(57, 57, 57)
+                .addComponent(LabelPagarSpinners, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(114, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(LabelParquimetroPrincipal)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(LabelParquimetroPrincipal)
+                        .addGap(0, 94, Short.MAX_VALUE))
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(LabelPagarSpinners)
                             .addGroup(layout.createSequentialGroup()
                                 .addGap(15, 15, 15)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -660,8 +854,7 @@ public class Terminal extends javax.swing.JFrame {
                                 .addComponent(BotonPagar)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                                 .addComponent(LabelPagar4)))
-                        .addGap(201, 201, 201)))
-                .addGap(0, 37, Short.MAX_VALUE))
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
         );
 
         pack();
@@ -790,7 +983,8 @@ public class Terminal extends javax.swing.JFrame {
                 JOptionPane.showMessageDialog(null, "No existe coche con ese ID en este parking", "ERROR", JOptionPane.ERROR_MESSAGE);
             } else {
                 // Si el ticket es válido y activo, mostramos la ventana de pago y cerramos la ventana de retiro
-                ventanaPagar();
+                
+                ventanaPagarSpinners();
                 pantallaRetirar.dispose();
             }
         }
@@ -825,35 +1019,35 @@ public class Terminal extends javax.swing.JFrame {
      */
     private void BotonPagarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BotonPagarActionPerformed
 
-        // Comprobaciones de que el campo de texto no esté vacío y que el valor ingresado sea un número válido
-        if (!validoNulos(TextPagar.getText().trim()) || !validoNumeros(TextPagar.getText().trim())) {
-            // Muestra un mensaje de error si no se ingresa un número válido
-            JOptionPane.showMessageDialog(null, "Asegurate de introducir el importe correctamente", "ERROR", JOptionPane.ERROR_MESSAGE);
-            return;  // Sale del método si la validación falla
-        }
-
-        // Comprobar si el dinero ingresado es suficiente para cubrir el importe correspondiente
-        if (app.totalDineroDevolver(app.calcularTiempoTranscurrido(ticketSeleccionado)) > Double.parseDouble(TextPagar.getText().trim())) {
-            // Muestra un mensaje de error si el dinero ingresado es insuficiente
-            JOptionPane.showMessageDialog(null, "El dinero introducido no alcanza el importe", "ERROR", JOptionPane.ERROR_MESSAGE);
-            return;  // Sale del método si el dinero no es suficiente
-        }
-        // Comprobar si el deposito tiene suficiente dinero como para dar cambio
-        if (Double.parseDouble(TextPagar.getText().trim()) > deposito.totalDeposito) {
-            JOptionPane.showMessageDialog(null, "No se puede dar suficiente cambio, por favor introduce una cantidad mas ajustada", "ERROR", JOptionPane.ERROR_MESSAGE);
-            return; // Sale del metodo si no tiene sufiente dinero
-        }
+//        // Comprobaciones de que el campo de texto no esté vacío y que el valor ingresado sea un número válido
+//        if (!validoNulos(TextPagar.getText().trim()) || !validoNumeros(TextPagar.getText().trim())) {
+//            // Muestra un mensaje de error si no se ingresa un número válido
+//            JOptionPane.showMessageDialog(null, "Asegurate de introducir el importe correctamente", "ERROR", JOptionPane.ERROR_MESSAGE);
+//            return;  // Sale del método si la validación falla
+//        }
+//
+//        // Comprobar si el dinero ingresado es suficiente para cubrir el importe correspondiente
+//        if (app.totalDineroDevolver(app.calcularTiempoTranscurrido(ticketSeleccionado)) > Double.parseDouble(TextPagar.getText().trim())) {
+//            // Muestra un mensaje de error si el dinero ingresado es insuficiente
+//            JOptionPane.showMessageDialog(null, "El dinero introducido no alcanza el importe", "ERROR", JOptionPane.ERROR_MESSAGE);
+//            return;  // Sale del método si el dinero no es suficiente
+//        }
+//        // Comprobar si el deposito tiene suficiente dinero como para dar cambio
+//        if (Double.parseDouble(TextPagar.getText().trim()) > deposito.totalDeposito) {
+//            JOptionPane.showMessageDialog(null, "No se puede dar suficiente cambio, por favor introduce una cantidad mas ajustada", "ERROR", JOptionPane.ERROR_MESSAGE);
+//            return; // Sale del metodo si no tiene sufiente dinero
+//        }
 
         // Calcula el cambio que se debe devolver
-        Double cambio = (Double.parseDouble(TextPagar.getText().trim())) - app.totalDineroDevolver(app.calcularTiempoTranscurrido(ticketSeleccionado));
+        Double cambio = total - app.totalDineroDevolver(app.calcularTiempoTranscurrido(ticketSeleccionado));
 
         // Llama al método devolverCambio para obtener las monedas de vuelta
         Deposito vueltas = deposito.devolverCambio(cambio);
 
         // Muestra un mensaje con el cambio que se va a devolver
-        JOptionPane.showMessageDialog(null, "Has introducido " + TextPagar.getText().trim() + " €, la vuelta correspondiente es: " + vueltas.toStringMonedas(vueltas), "VUELTAS", JOptionPane.PLAIN_MESSAGE);
+        JOptionPane.showMessageDialog(null, "Has introducido " + total + " €, la vuelta correspondiente es: " + vueltas.toStringMonedas(vueltas), "VUELTAS", JOptionPane.PLAIN_MESSAGE);
         // Actualiza el depósito con el dinero ingresado por el usuario
-        deposito.introducirDinero(TextPagar.getText().trim());
+        deposito.introducirDinero(String.valueOf(total));
 
         // Libera la plaza correspondiente, ya que el ticket ha sido pagado
         app.liberarPlaza(ticketSeleccionado);
@@ -865,7 +1059,7 @@ public class Terminal extends javax.swing.JFrame {
         ticketSeleccionado.desactivar();
 
         // Cierra la ventana de pago
-        pantallaPagar.dispose();
+        pantallaPagarSpinners.dispose();
 
     }//GEN-LAST:event_BotonPagarActionPerformed
 
@@ -916,6 +1110,7 @@ public class Terminal extends javax.swing.JFrame {
     private javax.swing.JLabel LabelHacienda;
     private javax.swing.JLabel LabelIntroducirId;
     private javax.swing.JLabel LabelPagar4;
+    private javax.swing.JLabel LabelPagarSpinners;
     private javax.swing.JLabel LabelParquimetroPrincipal;
     private javax.swing.JLabel LabelRetirarCoche;
     private javax.swing.JScrollPane ScrollTabla;
